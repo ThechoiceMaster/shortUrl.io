5324c8f7ba7b2ed170a4d3ef58bbfc21738784c3
My nikname "ton <68073926+ThechoiceMaster@users.noreply.github.com>
2022-08-30 01:11:14 +0700

    port domain

--- server.js
+++ server.js
@@ -8,11 +8,11 @@ app.use(bodyParser.json())
 const knex = require('knex')({
     client: 'mysql',
     connection: {
-      host : process.env.DB_HOST,
-      port : process.env.DB_PORT,
-      user : process.env.DB_USER,
-      password : process.env.DB_PASS,
-      database : process.env.DB_DATABASE
+      host: process.env.DB_HOST,
+      port: process.env.DB_PORT,
+      user: process.env.DB_USER,
+      password: process.env.DB_PASS,
+      database: process.env.DB_DATABASE
     },
     pool: { min: 0, max: 7 }
   });
@@ -22,7 +22,7 @@ const urlStore = {}
 app.set('view engine', 'ejs');
 
 app.get('/', (req, res) => {
-    app.render('./views/index')
+    res.render('index.ejs')
 })
 
 app.post('/', (req, res) => {
